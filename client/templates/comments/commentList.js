6c673b131db5407486b7cb3c417f18c6bf508869
Ben Diuguid <Bendiuguid@gmail.com>
2015-03-14 06:42:51 -0400

    I love coffee

---
+++ client/templates/comments/commentList.js
@@ -0,0 +1,22 @@
+Template.commentList.events({
+  "click #aliasSubmit": function(event) {
+    var name = $('#aliasText').val();
+
+    Session.set('aliasName', name);
+    return;
+  },
+  "click #sendComment": function(event) {
+    if(!this.comment) {
+      this.comment = [];
+    }
+    this.comment.push({author: Session.get('aliasName'), content: $('#commentText').val() });
+
+    Rooms.update(this._id, this);
+  }
+});
+
+Template.commentList.helpers({
+  alias: function() {
+    return Session.get('aliasName');
+  }
+});
